ae5f0d1df6692ea2da8376f863865e4a71dcffe7
Bruno Morgado <brmorg@gmail.com>
2021-09-14 19:32:12 -0400

    Create and Demonstrate MattedPhoto and FramedPhoto classes

--- photo.js
+++ photo.js
@@ -6,25 +6,80 @@ class Photo {
 
   price() {
     if (this.width === 8 && this.height == 10) {
-      return "$4";
+      return 4;
     } else if (this.width === 10 && this.height === 12) {
-      return "$6";
+      return 6;
     } else {
-      return "$10";
+      return 10;
     }
   }
 
   toString() {
     return `This is a ${this.width} by ${
       this.height
-    } photo and it costs ${this.price()}`;
+    } photo and it costs $${this.price()}`;
   }
 }
 
+class MattedPhoto extends Photo {
+  constructor(width, height, color) {
+    super(width, height);
+    this.color = color;
+  }
+
+  price() {
+    return super.price() + 10;
+  }
+
+  toString() {
+    return `This is a ${this.width} by ${this.height} matted ${
+      this.color
+    } photo and it costs $${this.price()}`;
+  }
+}
+
+class FramedPhoto extends Photo {
+  constructor(width, height, material, style) {
+    super(width, height);
+    this.material = material;
+    this.style = style;
+  }
+
+  price() {
+    return super.price() + 25;
+  }
+
+  toString() {
+    return `This is a ${this.width} by ${
+      this.height
+    } framed photo. The style is ${this.style} and it costs $${this.price()}`;
+  }
+}
+
+//Class photo objects
 let firstPhoto = new Photo();
 let secondPhoto = new Photo(10, 12);
 let thirdPhoto = new Photo(20, 25);
 
+//Child classes objects
+let mattedPhoto1 = new MattedPhoto(8, 10, "pink");
+let mattedPhoto2 = new MattedPhoto(10, 12, "brown");
+let mattedPhoto3 = new MattedPhoto(15, 20, "blue");
+
+let framedPhoto1 = new FramedPhoto(8, 10, "wood", "modern");
+let framedPhoto2 = new FramedPhoto(10, 12, "steel", "classic");
+let framedPhoto3 = new FramedPhoto(15, 20, "glass", "artistic");
+
+console.log("###############################################");
 console.log(firstPhoto.toString());
 console.log(secondPhoto.toString());
 console.log(thirdPhoto.toString());
+console.log("###############################################");
+console.log(mattedPhoto1.toString());
+console.log(mattedPhoto2.toString());
+console.log(mattedPhoto3.toString());
+console.log("###############################################");
+console.log(framedPhoto1.toString());
+console.log(framedPhoto2.toString());
+console.log(framedPhoto3.toString());
+console.log("###############################################");
